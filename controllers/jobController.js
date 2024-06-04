6fdc17e6323ebaca659e0e3b68085fcfdf426df3
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-06-04 10:24:56 +0100

    hello

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,48 +1,43 @@
 import Job from "../models/JobModel.js";
-
+import { StatusCodes } from "http-status-codes";
+import { NotFoundError } from "../errors/customErrors.js";
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
-  res.status(200).json({ jobs });
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const createJob = async (req, res) => {
   const job = await Job.create(req.body);
-  res.status(201).json({ job });
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 export const getSingleJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
   if (!job) {
-    return res.status(404).json({ message: `no job with ${id}` });
+    throw new NotFoundError(`no job with ${id}`);
   }
-  res.status(200).json({ job });
+  res.status(StatusCodes.OK).json({ job });
 };
 
 export const updateJob = async (req, res) => {
-  const { company, position } = req.body;
-  if (!company || !position) {
-    return res
-      .status(400)
-      .json({ message: "please provide company and position" });
-  }
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ message: `no job with ${id}` });
+  const updatedJob = await Job.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  if (!updatedJob) {
+    throw new NotFoundError(`no job with ${id}`);
   }
-  job.company = company;
-  job.position = position;
-  res.status(200).json({ job });
+  res.status(StatusCodes.OK).json({ job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
-  const job = jobs.find((job) => job.id === id);
-  if (!job) {
-    return res.status(404).json({ message: `no job with ${id}` });
+  const removedJob = await Job.findByIdAndDelete(id);
+  if (!removedJob) {
+    throw new NotFoundError(`no job with ${id}`);
   }
-  const newJobs = jobs.filter((job) => job.id !== id);
-  jobs = newJobs;
-  res.status(200).json({ message: "job deleted successfully", job });
+  res
+    .status(StatusCodes.OK)
+    .json({ message: "job deleted successfully", job: removedJob });
 };
--- server.js
+++ server.js
@@ -4,10 +4,14 @@ dotenv.config();
 import express from "express";
 const app = express();
 import morgan from "morgan";
-
+import mongoose from "mongoose";
+import { body, validationResult } from "express-validator";
 // routers
 import jobRouter from "./routes/jobRouter.js";
-import mongoose from "mongoose";
+
+// middleware
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -15,20 +19,31 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-app.post("/", (req, res) => {
-  console.log(req);
-  res.json({ message: "data received", data: req.body });
-});
+app.post(
+  "/api/v1/test",
+  [
+    body("name").notEmpty().withMessage("name is required"),
+    (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const errorMessages = errors.array().map((error) => error.message);
+      }
+      return res.status(400).json({ errors: errorMessages });
+    },
+    next(),
+  ],
+  (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `hello${name}`, data: req.body });
+  }
+);
 
 app.use("/api/v1/jobs", jobRouter);
 
 app.use("*", (req, res) => {
   return res.status(404).json({ message: "not found" });
 });
-app.use((err, req, res, next) => {
-  console.log(err);
-  return res.status(500).json({ message: "something went wrong" });
-});
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5100;
 try {
