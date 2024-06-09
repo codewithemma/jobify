4239c0536d8419196661c0df02c213f770ab191a
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-06-10 00:15:58 +0100

    ✔️

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customErrors.js";
 export const register = async (req, res) => {
   const isFirstUser = (await User.countDocuments()) === 0;
   req.body.role = isFirstUser ? "admin" : "user";
@@ -12,5 +13,12 @@ export const register = async (req, res) => {
     .json({ message: "user created successfully" });
 };
 export const login = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) throw new UnauthenticatedError("invalid credential");
+  const isPasswordCorrect = await comparePassword(
+    req.body.password,
+    user.password
+  );
+  if (!isPasswordCorrect) throw new UnauthenticatedError("invalid credential");
   res.send("login");
 };
