7a146a2a2be501d2863e7c02a5e2b386254c986f
Chikezie Ugochukwu Emmanuel <codewithemma@gmail.com>
2024-07-28 01:16:02 +0100

    almost done

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ import { loader as DashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
 import { loader as adminLoader } from "./pages/Admin";
+import { loader as statLoader } from "./pages/Stats";
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("dark-theme") === "true";
@@ -57,6 +58,7 @@ const router = createBrowserRouter([
           {
             path: "stats",
             element: <Stats />,
+            loader: statLoader,
           },
           {
             path: "all-jobs",
--- client/src/pages/Stats.jsx
+++ client/src/pages/Stats.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { customFetch } from "../utils/customFetch";
+import { useLoaderData } from "react-router-dom";
+import { ChartsContainer, StatsContainer } from "../components";
+
+export const loader = async () => {
+  try {
+    const response = await customFetch.get("/jobs/stats");
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+};
 
 const Stats = () => {
-  return <div>Stats</div>;
+  const { defaultStats, monthlyApplications } = useLoaderData();
+
+  return (
+    <>
+      <StatsContainer defaultStats={defaultStats} />
+      {monthlyApplications?.length > 1 && (
+        <ChartsContainer data={monthlyApplications} />
+      )}
+    </>
+  );
 };
 
 export default Stats;
